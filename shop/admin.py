b2f5aaa61dc435d68f876be842e395e1cdc25116
Edbeer <edbeermtn@gmail.com>
2021-09-25 23:00:18 +0700

    ordering models for main title and min_volimes for images

--- shop/admin.py
+++ shop/admin.py
@@ -1,11 +1,32 @@
 from django.contrib import admin
 from django import forms
-from django.forms import ModelChoiceField
+from django.core.exceptions import ValidationError
+from django.forms import ModelChoiceField, ModelForm
 
 from .models import *
 
+from PIL import Image
+
+
+class NotebookAdminForm(ModelForm):
+
+    MIN_RESOLUTION = (400, 400)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].help_text = 'Загружайте изображения с минимальным размером {}*{}'.format(*self.MIN_RESOLUTION)
+
+    def clean_image(self):
+        image = self.clean_image['image']
+        img = Image.open(image)
+        min_height, min_width = self.MIN_RESOLUTION
+        if img.height < min_height or img.width < min_width:
+            raise ValidationError('Разрешение иображения меньше минимального значения')
+        return image
 
 class NotebookAdmin(admin.ModelAdmin):
+
+    form = NotebookAdminForm
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
