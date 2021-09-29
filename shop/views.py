951f89c7af70f80e5a09b83f92b4927f1bf083ec
Edbeer <edbeermtn@gmail.com>
2021-09-29 23:25:27 +0700

    add mixins

--- shop/views.py
+++ shop/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, View
 
-from .models import Notebook, Smartphone, Category, CategoryManager
+from .models import Notebook, Smartphone, Category, LatestProduct
+
+from .mixins import CategoryDetailMixin
 
 
 def test(request):
     categories = Category.objects.get_categories_for_left_sidebar()
-    return render(request, 'shop/shop.html', {'categories': categories})
+    products = LatestProduct.objects.get_products_for_main_page()
+    return render(request, 'templates/base.html', {'categories': categories})
 
 
-class ProductDetailView(DetailView):
+class ProductDetailView(CategoryDetailMixin, DetailView):
 
     CT_MODEL_MODEL_CLASS = {
         'notebook': Notebook,
@@ -26,12 +29,15 @@ class ProductDetailView(DetailView):
     slug_url_kwarg = 'slug'
 
 
-def shop(request):
-    categories = Category.objects.get_categories_for_left_sidebar()
-    return render(request, 'shop/shop.html', {'categories': categories})
+class HomePage(View):
+    
+    def get(self, request):
+        categories = Category.objects.get_categories_for_left_sidebar()
+        products = LatestProduct.objects.get_products_for_main_page()
+        return render(request, 'shop/shop.html', {'categories': categories, 'products': products})
 
 
-class CategoryDetailView(DetailView):
+class CategoryDetailView(CategoryDetailMixin, DetailView):
 
     model = Category
     queryset = Category.objects.all()
